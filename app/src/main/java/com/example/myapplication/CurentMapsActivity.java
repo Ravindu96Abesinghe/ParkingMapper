38785b50b739e9d6d4bed4b3c151d2ef1c307e76
GayanJayarathne <krgayan97@gmail.com>
2023-04-29 17:31:35 +0530

    map created

--- app/src/main/java/com/example/myapplication/CurentMapsActivity.java
+++ app/src/main/java/com/example/myapplication/CurentMapsActivity.java
@@ -1,20 +1,109 @@
 package com.example.myapplication;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.widget.SearchView;
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
+import com.example.myapplication.databinding.ActivityCurentMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.myapplication.databinding.ActivityCurentMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
+import java.util.List;
 
+//
+//import androidx.fragment.app.FragmentActivity;
+//
+//import android.os.Bundle;
+//
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.SupportMapFragment;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
+//import com.example.myapplication.databinding.ActivityCurentMapsBinding;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
 public class CurentMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+//    private GoogleMap mMap;
+//    private ActivityCurentMapsBinding binding;
+//    private SearchView searchView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        binding = ActivityCurentMapsBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+//
+//        // Get reference to the search view
+//        searchView = findViewById(R.id.searchView);
+//
+//        // Set listener for search view
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                // Get location coordinates using Geocoding API
+//                LatLng location = getLocationFromAddress(query);
+//
+//                // Add a marker for the searched location
+//                mMap.addMarker(new MarkerOptions().position(location).title(query));
+//                mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+//
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return false;
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
+//
+//    private LatLng getLocationFromAddress(String address) {
+//        Geocoder geocoder = new Geocoder(this);
+//        List<Address> addressList;
+//        LatLng location = null;
+//        try {
+//            addressList = geocoder.getFromLocationName(address, 1);
+//            if (addressList != null && ((List<?>) addressList).size() > 0) {
+//                Address addr = addressList.get(0);
+//                location = new LatLng(addr.getLatitude(), addr.getLongitude());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return location;
+//    }
+
+
+
+
 
     private GoogleMap mMap;
     private ActivityCurentMapsBinding binding;
@@ -41,13 +130,22 @@ public class CurentMapsActivity extends FragmentActivity implements OnMapReadyCa
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Moratuwa, Sri Lanka and move the camera
+        LatLng moratuwa = new LatLng(6.7730, 79.8816);
+        mMap.addMarker(new MarkerOptions().position(moratuwa).title("Marker in Moratuwa, Sri Lanka"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(moratuwa));
     }
 }
--- app/src/main/java/com/example/myapplication/CurruntMap.java
+++ app/src/main/java/com/example/myapplication/CurruntMap.java
@@ -1,55 +1,84 @@
-package com.example.myapplication;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Bundle;
-import android.view.WindowManager;
-import android.widget.Toast;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
-
-public class CurruntMap extends AppCompatActivity {
-
-    SupportMapFragment supportMapFragment;
-    FusedLocationProviderClient fusedLocationProviderClient;
-
+//package com.example.myapplication;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.app.ActivityCompat;
+//
+//import android.Manifest;
+//import android.annotation.SuppressLint;
+//import android.content.pm.PackageManager;
+//import android.location.Location;
+//import android.os.Bundle;
+//import android.view.WindowManager;
+//import android.widget.Toast;
+//
+//import com.google.android.gms.location.FusedLocationProviderClient;
+//import com.google.android.gms.location.LocationServices;
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.SupportMapFragment;
+//import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.android.gms.tasks.Task;
+//import com.karumi.dexter.Dexter;
+//import com.karumi.dexter.PermissionToken;
+//import com.karumi.dexter.listener.PermissionDeniedResponse;
+//import com.karumi.dexter.listener.PermissionGrantedResponse;
+//import com.karumi.dexter.listener.PermissionRequest;
+//import com.karumi.dexter.listener.single.PermissionListener;
+//
+//public class CurruntMap extends AppCompatActivity {
+//
+//    SupportMapFragment supportMapFragment;
+//    FusedLocationProviderClient fusedLocationProviderClient;
+//
+////    @Override
+////    protected void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_currunt_map);
+////        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+////
+////        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.btnCLocation);
+////        fusedLocationProviderClient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
+////
+////        Dexter.withContext(getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+////                .withListener(new PermissionListener() {
+////                    @Override
+////                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+////                        getCurrentLocation();
+////
+////                    }
+////
+////                    @Override
+////                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+////
+////                    }
+////
+////                    @Override
+////                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+////                        permissionToken.continuePermissionRequest();
+////
+////                    }
+////                }).check();
+////    }
+//
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_currunt_map);
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 //
-//        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.btnCLocation);
-//        fusedLocationProviderClient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
+//        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 //
 //        Dexter.withContext(getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
 //                .withListener(new PermissionListener() {
 //                    @Override
 //                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
 //                        getCurrentLocation();
-//
 //                    }
 //
 //                    @Override
@@ -60,72 +89,43 @@ public class CurruntMap extends AppCompatActivity {
 //                    @Override
 //                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
 //                        permissionToken.continuePermissionRequest();
-//
 //                    }
 //                }).check();
 //    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_currunt_map);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        Dexter.withContext(getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                .withListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        getCurrentLocation();
-                    }
-
-                    @Override
-                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                        permissionToken.continuePermissionRequest();
-                    }
-                }).check();
-    }
-
-
-    public void getCurrentLocation() {
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-
-                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                    @Override
-                    public void onMapReady(@NonNull GoogleMap googleMap) {
-                        if(location != null){
-                            LatLng lating = new LatLng(location.getLatitude(),location.getLongitude());
-                            MarkerOptions markerOptions = new MarkerOptions().position(lating).title("Current Location");
-                            googleMap.addMarker(markerOptions);
-                            googleMap.animateCamera((CameraUpdateFactory.newLatLngZoom(lating,15)));
-
-                        }else{
-                            Toast.makeText(CurruntMap.this,"Please on Location App Permission",Toast.LENGTH_SHORT);
-                        }
-                    }
-                });
-            }
-        });
-    }
-}
+//
+//
+//    public void getCurrentLocation() {
+//
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+//        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//
+//                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+//                    @Override
+//                    public void onMapReady(@NonNull GoogleMap googleMap) {
+//                        if(location != null){
+//                            LatLng lating = new LatLng(location.getLatitude(),location.getLongitude());
+//                            MarkerOptions markerOptions = new MarkerOptions().position(lating).title("Current Location");
+//                            googleMap.addMarker(markerOptions);
+//                            googleMap.animateCamera((CameraUpdateFactory.newLatLngZoom(lating,15)));
+//
+//                        }else{
+//                            Toast.makeText(CurruntMap.this,"Please on Location App Permission",Toast.LENGTH_SHORT);
+//                        }
+//                    }
+//                });
+//            }
+//        });
+//    }
+//}
